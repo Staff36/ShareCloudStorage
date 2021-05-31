def375c55c3c2af76ddcdec3cc0ee4c2ba2f3668
STAFF <dima-tronin@mail.ru>
2021-05-31 15:08:28 +0300

    Modified mainFrame's design

--- FXClient/src/main/java/FXClient.java
+++ FXClient/src/main/java/FXClient.java
@@ -1,7 +1,9 @@
+import FrameControllers.MainFrameController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class FXClient extends Application {
@@ -9,9 +11,11 @@ public class FXClient extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FrameControllers/authFrame.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Share Cloud Storage: Authorization");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image(MainFrameController.class.getResourceAsStream("icons/AppIcon.png")))
         primaryStage.show();
+
     }
 
 
