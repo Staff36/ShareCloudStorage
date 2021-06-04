f9f56b33dfca3a8c8cac8902fff0fe2c806c5324
TroninDmitry <dima-tronin@mail.ru>
2021-06-05 01:22:14 +0300

    Finished
    It has few bugs, ill try to resolve it tomorrow but it looks imposible

---
+++ ServersStorage/admin/test/Array/src/geekbrains/Array/Main.java
@@ -0,0 +1,30 @@
+package geekbrains.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int arr1[];
+	int x=5;
+	arr1= new int[x];
+	for (int i=0; i<x;i++){
+	    arr1[i]=(int)(Math.random())*1000;
+    }
+    }
+	public static void qSorting (int[] arr, int x){
+	    int arrMin[];
+	    int arrMax[];
+	    int arrMed[];
+	    if (arr.length==1){
+	        
+        }
+
+	    for(int i=0;i<x;i++){
+
+        }
+
+        }
+
+
+
+    }
+
